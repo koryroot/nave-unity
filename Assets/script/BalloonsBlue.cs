d73566c442ca260a0c3c027e729a298230f1810e
Luis <Luistejeda0011@gmail.com>
2021-03-28 06:59:38 -0300

    W

---
+++ Assets/script/BalloonsBlue.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonsBlue : MonoBehaviour
+{
+    [SerializeField] Sprite[] balloonSprites;
+    [SerializeField] Vector3 force;
+
+
+    private Rigidbody2D rbb;
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        rbb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        spriteRenderer.sprite = balloonSprites[Random.Range(0, 1)];
+        transform.position = new Vector3(Random.Range(-8.7f, 6.4f), transform.position.y, transform.position.z);
+
+        force = new Vector3(0, Random.Range(155, 250), 0);
+        rbb.AddForce(force);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "topWall")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
